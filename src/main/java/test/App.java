9395b620a2c58aa33f01a87e2e1e6e5ee4ba15df
Jakob Homan <jghoman@gmail.com>
2016-03-10 14:36:38 -0800

    first commit

---
+++ src/main/java/test/App.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+
+    }
+}
